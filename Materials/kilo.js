66b7ba329c42e2de01530c7287faaa9d8d53aebc
CornSeller <166633175+CornSeller69@users.noreply.github.com>
2024-11-27 15:55:01 +0100

    updated files
    music and fixes

--- Materials/kilo.js
+++ Materials/kilo.js
@@ -2,6 +2,8 @@ let currentValue;
 let value;
 let score = 0;
 let highscore = 0;
+let music1 = new Audio('Materials/music1.mp3');
+music1.loop = true;
 document.getElementById("button1").addEventListener("click", play);
 document.getElementById("button2").addEventListener("click", displayHelp);
 // Wpisuj karty jako poniższe zmienne, aby było łatwiej zmieniać
@@ -25,5 +27,12 @@ function hideHelp() {
 }
 
 function play() {
-    return; //
+    document.getElementById("button1").style.opacity = 0;
+    document.getElementById("button2").style.opacity = 0;
+    setTimeout(function() {
+        document.getElementById("button1").style.visibility = 'hidden';
+        document.getElementById("button2").style.visibility = 'hidden';
+    },500);
+    music1.play();
+    debugger; /////
 }
