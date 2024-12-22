6f77f6e4930eac5cbd55132d6fcce6bc29eefc7f
CornSeller <166633175+CornSeller69@users.noreply.github.com>
2024-12-22 21:53:09 +0100

    File dump
    Already adding some files for Kilo, such as first cards

--- Materials/kilo.js
+++ Materials/kilo.js
@@ -4,13 +4,31 @@ let score = 0;
 let highscore = 0;
 let music1 = new Audio('Materials/music1.mp3');
 music1.loop = true;
-document.getElementById("button1").addEventListener("click", play);
-document.getElementById("button2").addEventListener("click", displayHelp);
+let isPolish = false;
+if(navigator.language == 'pl') {
+    isPolish = true;
+    translatePL();
+}
 // Wpisuj karty jako poniższe zmienne, aby było łatwiej zmieniać
 let qSpade = 'Materials/Cards/q-spade.png';
 let qClub = 'Materials/Cards/q-club.png';
 let qDiamond = 'Materials/Cards/q-diamond.png';
 let qHeart = 'Materials/Cards/q-heart.png';
+
+function translatePL() {
+    document.getElementById("button1").innerHTML = "Graj";
+    document.getElementById("button2").innerHTML = "Jak grać?";
+    document.getElementById("button3").innerHTML = "Zamknij";
+    document.getElementById("htph").innerHTML = "Jak grać w Kilo?";
+    document.getElementById("htpi").innerHTML = `Zdobywasz punkt jeżeli twój wybór 'Wyższa' lub 'Niższa' pasuje.<br>
+            Zdobędziesz punkt za 'Niższa' jeżeli następna karta jest niższa niż ta obecna, i zdobędziesz za 'Wyższa', jeżeli następna będzie wyższa lub równa niż ta obecna.<br>
+            Wartości kart odpowiadają numerkom na nich; dla Asa, Jopka, Damy i Króla jest trochę inaczej jednak. Gdyż As ma wartość 11 (tak, nie ma karty o wartości 1, najniższa to 2 w tej grze),<br>
+            ta reszta kart jest zwykle uznawana za *wartość większa niż 10, lecz mniejsza niż 11*. Ich kolejność: J &#60; Q &#60; K.`;
+    document.getElementById("score").innerHTML = "Wynik: 0";
+    document.getElementById("hscore").innerHTML = "Najwyższy wynik: 0";
+    document.getElementById("cimg").src = 'Materials/Cards/nopl.png';
+    //document.getElementById("").innerHTML = "";
+}
 
 function randomize() {
     return Math.floor(Math.random() * 52) + 1;
